7231b0be0b53a24c2a77ee430230587a2243e2f9
RizkyPratama05 <rizkypp428@gmail.com>
2025-12-05 15:20:19 +0700

    menambahkan companion object

--- app/src/main/java/com/example/pertemuan9/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan9/viewmodel/DetailViewModel.kt
@@ -31,3 +31,9 @@ class DetailViewModel (
     suspend fun deleteSiswa(){
         repositoriSiswa.deleteSiswa(uiDetailState.value.detailSiswa.toSiswa())
     }
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
