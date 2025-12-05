a6c956d03b8292fb1f19f3ccc3539ae273559e74
RizkyPratama05 <rizkypp428@gmail.com>
2025-12-05 15:00:19 +0700

    mengedit file RepositoriSiswa.kt

--- app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
@@ -7,6 +7,10 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
+
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+
+    suspend fun deleteSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
@@ -14,4 +18,7 @@ class OfflineRepositoriSiswa(
 ): RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
 }
